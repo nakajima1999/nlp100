67b90e43c057631fff4984db249ebe4c6374472e
nakajima1999 <s1711547@s.tsukuba.ac.jp>
2021-12-11 23:19:52 +0900

    chapter2

---
+++ chapter2/nlp100-15.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 第2章: UNIXコマンド
+# 15. 末尾のN行を出力
+# 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち末尾のN行だけを表示せよ．確認にはtailコマンドを用いよ．
+f_in = 'popular-names.txt'
+n = int(input())
+with open(f_in) as f:
+    lines = f.readlines()
+    out_lines = lines[-n:]
+
+for line in out_lines:
+    print(line.strip())
--- chapter2/nlp100-15.sh
+++ chapter2/nlp100-15.sh
@@ -5,5 +5,5 @@ echo -n INPUT_STR:
 # 入力を受付、その入力を「str」に代入
 read n 
 
-# 先頭のN行だけを表示
-head -n $n popular-names.txt
+# 末尾のN行だけを表示
+tail -n $n popular-names.txt
