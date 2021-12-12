78a2de7196583dccabd631a5db5d2246a1015ef0
nakajima1999 <s1711547@s.tsukuba.ac.jp>
2021-12-12 17:21:26 +0900

    chapter2

--- chapter2/nlp100-18.py
+++ chapter2/nlp100-18.py
@@ -9,4 +9,10 @@
 import pandas as pd
 
 f_in = 'popular-names.txt'
+df = pd.read_csv(f_in, sep='\t', header=None)
+df.columns = ['name', 'sex', 'number', 'year']
+df.sort_values(by='number', ascending=False, inplace=True)
+
+f_out = 'sort_col3.txt'
+df.to_csv(f_out, sep='\t', index=False, header=None)
 
