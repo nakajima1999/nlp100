4dbef3d9f9bb907ecaa64f1b13a8490aeb559355
nakajima1999 <s1711547@s.tsukuba.ac.jp>
2021-12-14 23:34:21 +0900

    chapter3

--- chapter3/nlp100_24.py
+++ chapter3/nlp100_24.py
@@ -6,8 +6,19 @@
 # 記事から参照されているメディアファイルをすべて抜き出せ．
 
 import nlp100_20
+import re
 
 if __name__ == '__main__':
     # 「イギリス」に関する記事本文
     text_uk = nlp100_20.get_text_uk()
-    print(text_uk)
+
+    # 記事から参照されているメディアファイル
+    pattern = r'\[\[(ファイル|File):([^]|]+?)(\|.*?)+\]\]'
+    
+    # 結果をすべて格納するリスト
+    all_result = re.findall(pattern, text_uk)
+
+    # 1件ずつ結果を出力
+    for result in all_result:
+        media_file = result[1]  # メディアファイル
+        print(media_file)
